f2bc721c8aea1973dad591044b281103e1bb7285
kofu145 <kofu145g@gmail.com>
2025-05-12 18:24:40 -0700

    feat: impl schlick's approximation

--- demo-tracer/material.h
+++ demo-tracer/material.h
@@ -63,7 +63,7 @@ class dielectric : public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
     
-            if (cannot_refract)
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double())
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -75,6 +75,13 @@ class dielectric : public material {
     private:
         // Refractive index in vacuum or air, or the ratio of the material's refractive index over the idx of enclosing media
         double refraction_index;
+
+        // using schlick approximation for reflectance
+        static double reflectance(double cosine, double refraction_index) {
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0*r0;
+            return r0 + (1-r0)*std::pow((1 - cosine), 5);
+        }
 };
 
 
