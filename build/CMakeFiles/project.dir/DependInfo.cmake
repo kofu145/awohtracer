ee82b60ca6b5898b370933947901cd6f4825fbbc
Kofu <kofu145g@gmail.com>
2025-08-04 23:15:22 -0700

    refactor: cmake build files

---
+++ build/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kofu/Projects/awohtracer/demo-tracer/rt_main.cpp" "CMakeFiles/project.dir/demo-tracer/rt_main.cpp.o" "gcc" "CMakeFiles/project.dir/demo-tracer/rt_main.cpp.o.d"
+  "" "project" "gcc" "CMakeFiles/project.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/link.d"
+  "CMakeFiles/project.dir/demo-tracer/rt_main.cpp.o"
+  "CMakeFiles/project.dir/demo-tracer/rt_main.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
