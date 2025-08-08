34876c582b05b25a70d360a599dba71fcde1c02b
Kofu <kofu145g@gmail.com>
2025-08-08 16:40:06 -0700

    feat(rt_main): introducing multiple test scenes in main

--- demo-tracer/rt_main.cpp
+++ demo-tracer/rt_main.cpp
@@ -1,25 +1,30 @@
-#include "rtweekend.h"
+#include "bvh.h"
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
-#include "sphere.h"
+#include "interval.h"
 #include "material.h"
-#include "camera.h"
-#include "bvh.h"
+#include "rtweekend.h"
+#include "sphere.h"
 #include "texture.h"
+#include <memory>
 
-int main() {
+void bouncing_spheres() {
     hittable_list world;
 
-    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(checker)));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1),
+                                                color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000,
+                                  make_shared<lambertian>(checker)));
 
-    //auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    //world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 0.9 * random_double(), 0.2,
+                          b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
@@ -29,17 +34,20 @@ int main() {
                     auto albedo = color::random() * color::random();
                     auto center2 = center + vec3(0, random_double(0, .5), 0);
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
+                    world.add(make_shared<sphere>(center, center2, 0.2,
+                                                  sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
                     auto fuzz = random_double(0, 0.5);
                     sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    world.add(
+                        make_shared<sphere>(center, 0.2, sphere_material));
                 } else {
                     // glass
                     sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    world.add(
+                        make_shared<sphere>(center, 0.2, sphere_material));
                 }
             }
         }
@@ -58,18 +66,57 @@ int main() {
 
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 50;
+    cam.max_depth = 50;
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+    cam.focus_dist = 10.0;
 
     cam.render(world);
-}
+};
+
+void checkered_spheres() {
+    hittable_list world;
+
+    auto checker =
+        make_shared<checker_texture>(.32, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10,
+                                  make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10,
+                                  make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+};
+
+int main() {
+    switch (2) {
+    case 1:
+        bouncing_spheres();
+        break;
+    case 2:
+        checkered_spheres();
+        break;
+    };
+};
