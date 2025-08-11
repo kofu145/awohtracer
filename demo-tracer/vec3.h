3464d5e9db6445e9c2cbee9b3e966b897d1439d5
Kofu <kofu145g@gmail.com>
2025-08-10 21:00:50 -0700

    style: clangd formatting

--- demo-tracer/vec3.h
+++ demo-tracer/vec3.h
@@ -4,115 +4,103 @@
 
 // basic vector, e stores x, y, z
 class vec3 {
-    public: 
+    public:
         double e[3];
         vec3() : e{0, 0, 0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        double x() const {return e[0];}
-        double y() const {return e[1];}
-        double z() const {return e[2];}
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
 
-        vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]); }
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
         double operator[](int i) const { return e[i]; }
-        double& operator[](int i) { return e[i]; }
+        double &operator[](int i) { return e[i]; }
 
-        vec3& operator += (const vec3& v){
+        vec3 &operator+=(const vec3 &v) {
             e[0] += v.e[0];
             e[1] += v.e[1];
             e[2] += v.e[2];
             return *this;
         }
 
-        vec3& operator*=(double t) {
+        vec3 &operator*=(double t) {
             e[0] *= t;
             e[1] *= t;
             e[2] *= t;
             return *this;
         }
 
+        vec3 &operator/=(double t) { return *this *= 1 / t; }
 
-        vec3& operator/=(double t) {
-            return *this *= 1/t;
-        }
-
-        double length() const {
-            return std::sqrt(length_squared());
-        }
+        double length() const { return std::sqrt(length_squared()); }
 
         double length_squared() const {
-            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
         }
 
         bool near_zero() const {
             // Return true if the vector is close to zero in all dimensions.
             auto s = 1e-8;
-            return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+            return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) &&
+                   (std::fabs(e[2]) < s);
         }
 
         static vec3 random() {
             return vec3(random_double(), random_double(), random_double());
         }
 
-        static vec3 random(double min, double max){
-            return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+        static vec3 random(double min, double max) {
+            return vec3(random_double(min, max), random_double(min, max),
+                        random_double(min, max));
         }
 };
 
-
-// point3 is just an alias for vec3, but useful for geometric clarity in the code.
+// point3 is just an alias for vec3, but useful for geometric clarity in the
+// code.
 using point3 = vec3;
-
 
 // Vector Utility Functions
 
-inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3& u, const vec3& v) {
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3& u, const vec3& v) {
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3& u, const vec3& v) {
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(const vec3& v, double t) {
-    return t * v;
+inline vec3 operator*(const vec3 &v, double t) { return t * v; }
+
+inline vec3 operator/(const vec3 &v, double t) { return (1 / t) * v; }
+
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
-inline vec3 operator/(const vec3& v, double t) {
-    return (1/t) * v;
-}
-
-inline double dot(const vec3& u, const vec3& v) {
-    return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
-}
-
-inline vec3 cross(const vec3& u, const vec3& v) {
+inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(const vec3& v) {
-    return v / v.length();
-}
+inline vec3 unit_vector(const vec3 &v) { return v / v.length(); }
 
 // simple rejection method to get random vector inside a unit sphere
 inline vec3 random_unit_vector() {
-    while (true){
+    while (true) {
         auto p = vec3::random(-1, 1);
         auto lensq = p.length_squared();
         // 1e-160 is for floating point abstraction leak so we don't
@@ -123,7 +111,7 @@ inline vec3 random_unit_vector() {
 }
 
 // same hemisphere check as checking rebound rays on intersect
-inline vec3 random_on_hemisphere(const vec3& normal) {
+inline vec3 random_on_hemisphere(const vec3 &normal) {
     vec3 on_unit_sphere = random_unit_vector();
     if (dot(on_unit_sphere, normal) > 0.0) // same hemisphere as normal
         return on_unit_sphere;
@@ -131,14 +119,15 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
         return -on_unit_sphere;
 }
 
-inline vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2*dot(v, n)*n;
+inline vec3 reflect(const vec3 &v, const vec3 &n) {
+    return v - 2 * dot(v, n) * n;
 }
 
-inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+inline vec3 refract(const vec3 &uv, const vec3 &n, double etai_over_etat) {
     auto cos_theta = std::fmin(dot(-uv, n), 1.0);
-    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
-    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel =
+        -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
     return r_out_perp + r_out_parallel;
 }
 
