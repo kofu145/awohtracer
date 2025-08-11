ad8e5c847f6526ba2cff8f7b4949bcd90600c30e
Kofu <kofu145g@gmail.com>
2025-08-10 20:59:24 -0700

    feat(sphere): method to get sphere uv coordinates

--- demo-tracer/sphere.h
+++ demo-tracer/sphere.h
@@ -5,25 +5,33 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& static_center, double radius, shared_ptr<material> mat) : center(static_center, vec3(0, 0, 0)), radius(std::fmax(0, radius)), mat(mat) {
+        sphere(const point3 &static_center, double radius,
+               shared_ptr<material> mat)
+            : center(static_center, vec3(0, 0, 0)),
+              radius(std::fmax(0, radius)), mat(mat) {
             auto rvec = vec3(radius, radius, radius);
             bbox = aabb(static_center - rvec, static_center + rvec);
         }
 
         aabb bounding_box() const override { return bbox; }
-        
-        sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat) : center(center1, center2-center1), radius(std::fmax(0, radius)), mat(mat) {
-            // bounding box around the ENTIRE possible range of motion for the sphere
+
+        sphere(const point3 &center1, const point3 &center2, double radius,
+               shared_ptr<material> mat)
+            : center(center1, center2 - center1), radius(std::fmax(0, radius)),
+              mat(mat) {
+            // bounding box around the ENTIRE possible range of motion for the
+            // sphere
             auto rvec = vec3(radius, radius, radius);
             aabb box1(center.at(0) - rvec, center.at(0) + rvec);
             aabb box2(center.at(1) - rvec, center.at(1) + rvec);
             bbox = aabb(box1, box2);
         }
-        
-        bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
+
+        bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
             point3 current_center = center.at(r.time());
-            vec3 oc = current_center - r.origin(); // C vec - eye origin point - (c - P(t))
-            
+            vec3 oc = current_center -
+                      r.origin(); // C vec - eye origin point - (c - P(t))
+
             /*
             quadratic formula calc
             // following is quadratic formula members a, b, c to find t
@@ -35,18 +43,18 @@ class sphere : public hittable {
             */
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
-            auto c = oc.length_squared() - radius*radius;
+            auto c = oc.length_squared() - radius * radius;
 
-            auto discriminant = h*h - a*c;
+            auto discriminant = h * h - a * c;
 
-            if (discriminant < 0){ // not hit
+            if (discriminant < 0) { // not hit
                 return false;
             }
 
             auto sqrtd = std::sqrt(discriminant);
 
             auto root = (h - sqrtd) / a;
-            if (!ray_t.surrounds(root)){
+            if (!ray_t.surrounds(root)) {
                 root = (h + sqrtd) / a;
                 if (!ray_t.surrounds(root))
                     return false;
@@ -57,11 +65,29 @@ class sphere : public hittable {
             // rec.normal = (rec.p - center) / radius;
             vec3 outward_normal = (rec.p - current_center) / radius;
             rec.set_face_normal(r, outward_normal);
+            get_sphere_uv(outward_normal, rec.u, rec.v);
             rec.mat = mat;
 
             return true;
         }
+
     private:
+        static void get_sphere_uv(const point3 &p, double &u, double &v) {
+
+            // p: a given point on the sphere of radius one, centered at the
+            // origin. u: returned value [0,1] of angle around the Y axis from
+            // X=-1. v: returned value [0,1] of angle from Y=-1 to Y=+1.
+            //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00
+            //     0.50> <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields
+            //     <0.50 0.00> <0 0 1> yields <0.25 0.50>       < 0  0 -1>
+            //     yields <0.75 0.50>
+
+            auto theta = std::acos(-p.y());
+            auto phi = std::atan2(-p.z(), p.x()) + pi;
+
+            u = phi / (2 * pi);
+            v = theta / pi;
+        }
         ray center;
         double radius;
         shared_ptr<material> mat;
