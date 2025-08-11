a4b2d654faf9ebd6a0af4d8c5c692f8bb9932bd6
Kofu <kofu145g@gmail.com>
2025-08-10 21:00:39 -0700

    feat(texture): image texture support and perlin texture support

--- demo-tracer/texture.h
+++ demo-tracer/texture.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "color.h"
+#include "perlin.h"
+#include "rtw_stb_image.h"
 
 class texture {
     public:
-    virtual ~texture() = default;
-    virtual color value(double u, double v, const point3& p) const = 0;
-
+        virtual ~texture() = default;
+        virtual color value(double u, double v, const point3 &p) const = 0;
 };
 
 class solid_color : public texture {
     public:
-        solid_color(const color& albedo) : albedo(albedo) {}
-        solid_color(double red, double green, double blue) : solid_color(color(red,green,blue)) {}
-        color value(double u, double v, const point3& p) const override {
+        solid_color(const color &albedo) : albedo(albedo) {}
+        solid_color(double red, double green, double blue)
+            : solid_color(color(red, green, blue)) {}
+        color value(double u, double v, const point3 &p) const override {
             return albedo;
         }
 
@@ -22,25 +24,64 @@ class solid_color : public texture {
 
 class checker_texture : public texture {
     public:
-        checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd)
-            : inv_scale(1.0/scale), even(even), odd(odd) {} 
+        checker_texture(double scale, shared_ptr<texture> even,
+                        shared_ptr<texture> odd)
+            : inv_scale(1.0 / scale), even(even), odd(odd) {}
 
-        checker_texture(double scale, const color& c1, const color& c2)
-            : checker_texture(scale, make_shared<solid_color>(c1), make_shared<solid_color>(c2)) {}
+        checker_texture(double scale, const color &c1, const color &c2)
+            : checker_texture(scale, make_shared<solid_color>(c1),
+                              make_shared<solid_color>(c2)) {}
 
-        color value(double u, double v, const point3& p) const override {
+        color value(double u, double v, const point3 &p) const override {
             auto xInteger = int(std::floor(inv_scale * p.x()));
             auto yInteger = int(std::floor(inv_scale * p.y()));
             auto zInteger = int(std::floor(inv_scale * p.z()));
-            
+
             bool isEven = (xInteger + yInteger + zInteger) % 2 == 0;
 
             return isEven ? even->value(u, v, p) : odd->value(u, v, p);
         }
 
-
     private:
         double inv_scale;
         shared_ptr<texture> even;
         shared_ptr<texture> odd;
+};
+
+class image_texture : public texture {
+    public:
+        image_texture(const char *filename) : image(filename) {}
+        color value(double u, double v, const point3 &p) const override {
+            // return cyan if no image texture
+            if (image.height() <= 0)
+                return color(0, 1, 1);
+
+            // clamp input tex coords to 0,1 and 1,0
+            u = interval(0, 1).clamp(u);
+            v = 1.0 - interval(0, 1).clamp(v);
+
+            auto i = int(u * image.width());
+            auto j = int(v * image.height());
+            auto pixel = image.pixel_data(i, j);
+
+            auto color_scale = 1.0 / 255.0;
+            return color(color_scale * pixel[0], color_scale * pixel[1],
+                         color_scale * pixel[2]);
+        }
+
+    private:
+        rtw_image image;
+};
+
+class noise_texture : public texture {
+    public:
+        noise_texture(double scale) : scale(scale) {}
+
+        color value(double u, double v, const point3 &p) const override {
+            return color(1, 1, 1) * 0.5 * (1.0 + noise.noise(scale * p));
+        }
+
+    private:
+        perlin noise;
+        double scale;
 };
