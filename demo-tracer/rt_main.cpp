1228a0fbba9b6548d438c4b37fc44b74ad2d2a2a
Kofu <kofu145g@gmail.com>
2025-08-23 16:21:33 -0700

    feat(driver): added new scenes to account for quads, diffuse lighting, texture tests/changes, etc

--- demo-tracer/rt_main.cpp
+++ demo-tracer/rt_main.cpp
@@ -5,6 +5,7 @@
 #include "hittable_list.h"
 #include "interval.h"
 #include "material.h"
+#include "quad.h"
 #include "rtweekend.h"
 #include "sphere.h"
 #include "texture.h"
@@ -16,10 +17,11 @@ void bouncing_spheres() {
     auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1),
                                                 color(.9, .9, .9));
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000,
-                                  make_shared<lambertian>(checker)));
+                                  make_shared<lambertian>(color(.5, .5, .5))));
 
     // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    auto offset = vec3(0.15, 0.15, 0.15);
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
@@ -35,47 +37,51 @@ void bouncing_spheres() {
                     auto albedo = color::random() * color::random();
                     auto center2 = center + vec3(0, random_double(0, .5), 0);
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, center2, 0.2,
-                                                  sphere_material));
+                    // world.add(make_shared<sphere>(center, center2, 0.2,
+                    // sphere_material));
+                    world.add(
+                        box(center - offset, center + offset, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
                     auto fuzz = random_double(0, 0.5);
                     sphere_material = make_shared<metal>(albedo, fuzz);
                     world.add(
-                        make_shared<sphere>(center, 0.2, sphere_material));
+                        box(center - offset, center + offset, sphere_material));
                 } else {
                     // glass
                     sphere_material = make_shared<dielectric>(1.5);
                     world.add(
-                        make_shared<sphere>(center, 0.2, sphere_material));
+                        box(center - offset, center + offset, sphere_material));
                 }
             }
         }
     }
-
+    auto large_offset = vec3(.7, .7, .7);
     auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    world.add(box(point3(0, 1, 0) - large_offset,
+                  point3(0, 1, 0) + large_offset, material1));
 
     auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    // world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    world.add(box(point3(-4, 1, 0) - large_offset,
+                  point3(-4, 1, 0) + large_offset, material2));
 
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    auto earth_texture = make_shared<image_texture>("res/test");
-    auto earth_surface = make_shared<lambertian>(earth_texture);
-    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
-    world.add(globe);
+    // world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    world.add(box(point3(4, 1, 0) - large_offset,
+                  point3(4, 1, 0) + large_offset, material3));
 
     world = hittable_list(make_shared<bvh_node>(world));
 
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 800;
+    cam.samples_per_pixel = 300;
     cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
 
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
@@ -105,6 +111,7 @@ void checkered_spheres() {
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
 
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
@@ -127,6 +134,7 @@ void earth() {
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
 
     cam.vfov = 20;
     cam.lookfrom = point3(15, 0, 12);
@@ -153,6 +161,7 @@ void perlin_spheres() {
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
 
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
@@ -164,8 +173,125 @@ void perlin_spheres() {
     cam.render(world);
 };
 
+void quads() {
+    hittable_list world;
+
+    // Materials
+    auto left_red = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3, -2, 5), vec3(0, 0, -4),
+                                vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2, -2, 0), vec3(4, 0, 0), vec3(0, 4, 0),
+                                back_green));
+    world.add(make_shared<quad>(point3(3, -2, 1), vec3(0, 0, 4), vec3(0, 4, 0),
+                                right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4),
+                                upper_orange));
+    world.add(make_shared<quad>(point3(-2, -3, 5), vec3(4, 0, 0),
+                                vec3(0, 0, -4), lower_teal));
+
+    camera cam;
+
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
+
+    cam.vfov = 80;
+    cam.lookfrom = point3(0, 0, 9);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
+void example_sc() {
+    hittable_list world;
+
+    auto material_ground = make_shared<lambertian>(color(0.5, 0.5, 0.8));
+    auto material_center = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    auto material_left = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right = make_shared<metal>(color(0.5, 0.5, 0.5), 0.0);
+
+    world.add(
+        make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(
+        make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(
+        make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.background = color(0.70, 0.80, 1.00);
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(0, 2, 4);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(world));
+}
+void simple_light() {
+    hittable_list world;
+    auto tex = make_shared<image_texture>("res/earthmap.jpg");
+    auto tex_mat = make_shared<lambertian>(tex);
+
+    auto uw_tex = make_shared<image_texture>("res/UW.png");
+    auto uw_mat = make_shared<lambertian>(uw_tex);
+
+    auto pertext = make_shared<noise_texture>(4);
+    world.add(
+        make_shared<sphere>(point3(0, -1000, 0), 1000,
+                            make_shared<lambertian>(color(0.5, 0.5, 0.8))));
+    world.add(make_shared<sphere>(point3(0, 2, 0), 2,
+                                  make_shared<lambertian>(pertext)));
+    world.add(make_shared<sphere>(point3(0, 2, 5), 2, tex_mat));
+    auto box_center = point3(0, 2, 10);
+    auto box_offset = point3(2, 2, 2);
+    world.add(box(box_center - box_offset, box_center + box_offset, uw_mat));
+
+    auto difflight = make_shared<diffuse_light>(color(10, 10, 10));
+    world.add(make_shared<quad>(point3(3, 1, -4), vec3(5, 0, 0), vec3(0, 5, 0),
+                                difflight));
+    world.add(make_shared<sphere>(point3(0, 7, 0), 2, difflight));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 1000;
+    cam.max_depth = 50;
+    cam.background = color(0, 0, 0);
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(40, 4, 12);
+    cam.lookat = point3(0, 2, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    switch (4) {
+    switch (7) {
     case 1:
         bouncing_spheres();
         break;
@@ -177,6 +303,17 @@ int main() {
         break;
     case 4:
         perlin_spheres();
+        break;
+    case 5:
+        example_sc();
+        break;
+
+    case 6:
+        quads();
+        break;
+
+    case 7:
+        simple_light();
         break;
     };
 };
